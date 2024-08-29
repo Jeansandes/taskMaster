0bb28b62b0cfcf40c1c6ec8991d225d5dff91042
Jean Sandes <sandesjean.sandes@gmail.com>
2024-08-29 17:21:21 -0300

    feat: create repositories

---
+++ src/main/java/com/sandesdev/taskMaster/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.sandesdev.taskMaster.repositories;
+
+import com.sandesdev.taskMaster.models.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+}
