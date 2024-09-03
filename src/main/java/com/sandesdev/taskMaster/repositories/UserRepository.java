408f023023d660efc4b3ee850d181e788765b6aa
Jean Sandes <sandesjean.sandes@gmail.com>
2024-09-02 22:14:37 -0300

    feat: implemetando novo método no UserRepository

--- src/main/java/com/sandesdev/taskMaster/repositories/UserRepository.java
+++ src/main/java/com/sandesdev/taskMaster/repositories/UserRepository.java
@@ -4,8 +4,12 @@ import com.sandesdev.taskMaster.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByName(String name);
+
+    Optional<UserModel> findByEmail(String email);
 }
